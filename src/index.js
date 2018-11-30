0139481106482bf689878383693732b99f783171
Eddie Webb <ollitech@gmail.com>
2018-11-30 12:49:06 -0500

    Pulls token from env var, working auth

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import GraphiQL from 'graphiql';
 import './index.css';
 import '../node_modules/graphiql/graphiql.css'
 
-console.log(process.env.CIRCLECI_API_TOKEN)
-var base64AuthValue = Buffer.from(process.env.CIRCLECI_API_TOKEN + ":").toString('base64');
-console.log(base64AuthValue);
 
 
 function graphQLFetcher(graphQLParams) {
 	console.log(graphQLParams);
   return fetch('/graphql-unstable', {
     method: 'post',
-    headers: { 'Content-Type': 'application/json','Authorization':'Basic NNEEEEED BASE^$ VALUE HERE' },
+    headers: { 'Content-Type': 'application/json','Authorization': process.env.REACT_APP_SECRET },
     body: JSON.stringify(graphQLParams),
   }).then(response => response.json());
 }
